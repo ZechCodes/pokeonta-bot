1b93e9f0a6cdd95bc136b5e2f56516fd35abfe53
Zech Zimmerman <hi@zech.codes>
2020-07-25 08:47:26 -0400

    Remove db code

--- pokeonta/__main__.py
+++ pokeonta/__main__.py
@@ -4,5 +4,4 @@ import pokeonta.bootstrap
 logger = pokeonta.bootstrap.setup_logger()
 client = pokeonta.bootstrap.create_bot(logger)
 pokeonta.bootstrap.load_cogs(client, logger)
-pokeonta.bootstrap.connect_db(logger)
 pokeonta.bootstrap.run(client, logger)
