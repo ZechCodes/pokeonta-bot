73d57d6ae29ca66e0b3f50dd672eac0af244a1c3
Zech Zimmerman <hi@zech.codes>
2020-07-28 19:59:57 -0400

    Move db connect to after when cogs are loaded

--- pokeonta/__main__.py
+++ pokeonta/__main__.py
@@ -2,7 +2,7 @@ import pokeonta.bootstrap
 
 
 logger = pokeonta.bootstrap.setup_logger()
-pokeonta.bootstrap.connect_db(logger)
 client = pokeonta.bootstrap.create_bot(logger)
 pokeonta.bootstrap.load_cogs(client, logger)
+pokeonta.bootstrap.connect_db(logger)
 pokeonta.bootstrap.run(client, logger)
