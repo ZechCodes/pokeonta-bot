bfdba3c5b6d9df21b5a7d983eae5e5d0158842cd
Zech Zimmerman <hi@zech.codes>
2020-07-28 19:05:34 -0400

    Connect the DB in bootstrap

--- pokeonta/__main__.py
+++ pokeonta/__main__.py
@@ -2,6 +2,7 @@ import pokeonta.bootstrap
 
 
 logger = pokeonta.bootstrap.setup_logger()
+pokeonta.bootstrap.connect_db(logger)
 client = pokeonta.bootstrap.create_bot(logger)
 pokeonta.bootstrap.load_cogs(client, logger)
 pokeonta.bootstrap.run(client, logger)
