3acc56b5c43edaa25b0b826214c2eeca2df68339
Zech Zimmerman <hi@zech.codes>
2020-08-01 21:19:35 -0400

    Add scheduler

--- pokeonta/__main__.py
+++ pokeonta/__main__.py
@@ -1,8 +1,10 @@
 import pokeonta.bootstrap
+import pokeonta.scheduler
 
 
 logger = pokeonta.bootstrap.setup_logger()
 client = pokeonta.bootstrap.create_bot(logger)
 pokeonta.bootstrap.load_cogs(client, logger)
 pokeonta.bootstrap.connect_db(logger)
+pokeonta.scheduler.initialize_scheduler(client.loop)
 pokeonta.bootstrap.run(client, logger)
