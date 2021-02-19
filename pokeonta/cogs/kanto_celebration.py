9bd23f6a87ea0ced4c39cb3d45481f714e16677a
Zech Zimmerman <hi@zech.codes>
2021-02-19 09:26:05 -0500

    Add location times for the Kanto Celebrations

--- pokeonta/cogs/kanto_celebration.py
+++ pokeonta/cogs/kanto_celebration.py
@@ -13,107 +13,34 @@ class KantoCelebrationCog(Cog):
         super().__init__(client)
         self.hourly_features = [
             Habitat(
-                name="Battle",
-                pokemon={
-                    "Machop": True,
-                    "Alolan Grimer": True,
-                    "Dratini": True,
-                    "Sneasel": True,
-                    "Skarmory": True,
-                    "Slakoth": True,
-                    "Sableye": True,
-                    "Meditite": True,
-                    "Swablu": True,
-                    "Zangoose": True,
-                    "Seviper": True,
-                    "Gible": True,
-                    "Croagunk": True,
-                    "Stunfisk": False,
-                    "**Durant**": True,
-                },
-                times=[],
+                name="Pallet Town",
+                pokemon={},
+                times=[9, 14],
             ),
             Habitat(
-                name="Friendship",
-                pokemon={
-                    "Pikachu": True,
-                    "Clefairy": True,
-                    "Jigglypuff": True,
-                    "Chansey": True,
-                    "Eevee": True,
-                    "Snorlax": False,
-                    "Togetic": False,
-                    "Marill": True,
-                    "Sudowoodo": True,
-                    "Wobbuffet": True,
-                    "Mantine": False,
-                    "Roselia": True,
-                    "Feebas": True,
-                    "Chimecho": False,
-                    "**Woobat**": True,
-                },
-                times=[13],
+                name="Pewter City",
+                pokemon={},
+                times=[10, 15],
             ),
             Habitat(
-                name="Fire",
-                pokemon={
-                    "Charmander": True,
-                    "Charizard": False,
-                    "Vulpix": False,
-                    "Ponyta": True,
-                    "Alolan Marowak": True,
-                    "Magmar": True,
-                    "Flareon": False,
-                    "Houndour": True,
-                    "Torchic": True,
-                    "Numel": False,
-                    "Tepig": False,
-                    "Darumaka": False,
-                    "Litwick": False,
-                    "**Heatmor**": True,
-                },
-                times=[11],
+                name="Cerulean City",
+                pokemon={},
+                times=[11, 16],
             ),
             Habitat(
-                name="Water",
-                pokemon={
-                    "Squirtle": True,
-                    "Blastoise": False,
-                    "Poliwag": True,
-                    "Tentacool": True,
-                    "Slowpoke": False,
-                    "Magikarp": True,
-                    "Chinchou": True,
-                    "**Qwilfish**": True,
-                    "Mudkip": True,
-                    "Carvanha": True,
-                    "Clamperl": True,
-                    "Oshawott": True,
-                    "Tympole": False,
-                    "Alomomola": False,
-                },
-                times=[12],
+                name="Fuschia City",
+                pokemon={},
+                times=[12, 17],
             ),
             Habitat(
-                name="Grass",
-                pokemon={
-                    "Bulbasaur": True,
-                    "Venusaur": False,
-                    "Oddish": True,
-                    "Exeggcute": True,
-                    "Alolan Exeggcutor": True,
-                    "**Tangela**": True,
-                    "Sunkern": True,
-                    "Treecko": True,
-                    "Seedot": True,
-                    "Cherrim": False,
-                    "Snover": True,
-                    "Leafeon": False,
-                    "Snivy": False,
-                    "Foongus": False,
-                    "Ferroseed": False,
-                },
-                times=[],
+                name="Pokemon League",
+                pokemon={},
+                times=[13, 18],
+            ),
+            Habitat(
+                name="All Areas",
+                pokemon={},
+                times=[19],
             ),
         ]
 
@@ -123,10 +50,11 @@ class KantoCelebrationCog(Cog):
         await self.send_habitat_change(now.hour, current=True)
 
     async def ready(self):
+        print(datetime.datetime.utcnow() - datetime.timedelta(hours=5))
         if datetime.datetime.utcnow() - datetime.timedelta(hours=5) < datetime.datetime(
             2021, 2, 20, 20, 0, 0, 0
         ):
-            self.logger.debug("Scheduling habitat change")
+            self.logger.debug("Scheduling location change")
             asyncio.get_event_loop().create_task(self.schedule_habitat_change())
 
     @Cog.command(aliases=["v"])
@@ -163,23 +91,22 @@ class KantoCelebrationCog(Cog):
         await ctx.send(message)
 
     async def schedule_habitat_change(self):
-        return
         now = datetime.datetime.utcnow() - datetime.timedelta(hours=5)
         next_change = now.replace(hour=now.hour + 1, minute=0, second=0, microsecond=0)
         self.logger.debug(
-            f"Next habitat change in {(next_change - now).seconds} for hour {now.hour + 1}"
+            f"Next location change in {(next_change - now).seconds} for hour {now.hour + 1}"
         )
         await asyncio.sleep((next_change - now).seconds)
 
         await self.send_habitat_change(now.hour + 1)
         if now + datetime.timedelta(hours=2) < datetime.datetime(
-            2020, 8, 16, 20, 0, 0, 0
+            2021, 2, 20, 20, 0, 0, 0
         ):
             await asyncio.sleep(60)
             asyncio.get_event_loop().create_task(self.schedule_habitat_change())
 
     async def send_habitat_change(self, hour: int, current: bool = False):
-        self.logger.debug(f"Looking for habitat in hour {hour}")
+        self.logger.debug(f"Looking for location in hour {hour}")
         for habitat in self.hourly_features:
             if hour in habitat.times:
                 await self.send_habitat_message(habitat, current)
@@ -188,12 +115,12 @@ class KantoCelebrationCog(Cog):
     async def send_habitat_message(self, habitat: Habitat, current: bool = False):
         guild = self.client.get_guild(340162408498593793)
         channel: discord.TextChannel = discord.utils.get(
-            guild.channels, name="go-fest-aug-16"
+            guild.channels, name="testing-bot"
         )
         embed = discord.Embed(
-            title=f"We're currently in the {habitat.name} habitat"
+            title=f"We're currently in {habitat.name}"
             if current
-            else f"We're now in the {habitat.name} Habitat!!!"
+            else f"We're now in {habitat.name}!!!"
         ).add_field(
             name="Featured Shinies!!!",
             value=", ".join(
